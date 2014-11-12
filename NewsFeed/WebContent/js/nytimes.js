44ea129d88edf439519d694a94cec74202d0e17d
rkanta <kanta.rajesh@gmail.com>
2014-11-11 21:27:32 -0500

    adding different ajax calls to resolve the cross-domain issue

--- NewsFeed/WebContent/js/nytimes.js
+++ NewsFeed/WebContent/js/nytimes.js
@@ -24,56 +24,28 @@ var main = function(){
     
  
   
-  
+  /*
     
     $("#rss-styled").rss("http://www.recruiter.com/feed/career.xml", {
         limit: 5,
         layoutTemplate: '<dl class="dl-horizontal">{entries}</dl>',
         entryTemplate: '<dt><a href="{url}">{title}</a></dt><dd>{shortBodyPlain}[{author}@{date}]</dd>'
-    }).show();
+    }).show();*/
     
-    /*
     
-    $("#rss-styled1").rss("http://www.recruiter.com/feed/career.xml", {
-        limit: 5,
-        layoutTemplate: '<dl class="dl-horizontal">{entries}</dl>',
-        entryTemplate: '<dt><a href="{url}">{title}</a></dt><dd>{shortBodyPlain}[{author}@{date}]</dd>'
-    }).show();
-    */
-    /*
-    $('#titulares').append("<li>" + titulo + "</li>");
-    $("#rssURL").val("http://feeds.encosia.com/Encosia");
-    loadData();
-});
-        
-function loadData()
-{           
-    $("#feedContainer").empty();
-    
-    $.get("FeedServer.ashx?url=" + $("#rssURL").val(), function(data) {                         
-        
-        $(data).find('item').each(function() {
-            var $item = $(this);
-            var title = $item.find('title').text();
-            var link = $item.find('link').text();
-            var description = $item.find('description').text();
-            var pubDate = $item.find('pubDate').text();
-
-            var html = "<div style=\"margin-bottom:8px;\">";
-            html += "<h3>" + title + "</h3>";
-            html += "<em>" + pubDate + "</em>";
-            html += "<p>" + description + "</p>";
-            html += "<a href=\"" + link + "\" target=\"_blank\">Read More</a>";
-            html += "</div>";
-
-            $('#feedContainer').append(html);
-        });
-    });
-    */
     //below code for RSS feed using yahoo api
     //default always shows the CNN Top stories
     $('#rss-styled1').empty();
     rssReader('http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml');
+    
+    var apikey = "A0SXFX54S3K0OC9GNCXG";
+    var baseUrl = "http://sandbox.feedly.com";
+    //var baseUrl = "http://localhost:8080/feedly";
+    //var URI = "/v3/search/feeds/";
+    var URI = "/v3/search/feeds/";
+    var query = "NDTV";
+    var url = baseUrl + URI+ "?apikey="+apikey+"&query="+query+ "&callback=?";
+    rssReader(url);
     
     $("#myid li").click(function() {
         var index = $(this).index();
@@ -121,6 +93,91 @@ function loadData()
     	
         var site = element;
         var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + site + '"') + '&format=xml';
+       
+        
+           /* $.getJSON(site + "?callback=?", function(json) { http://api.themoviedb.org/2.1/Movie.search/en
+                console.log(json);}); http://api.themoviedb.org/3/movie/popular/en/json/23afca60ebf72f8d88cdcae2c4f31866/
+                /v3/search/feeds
+                 * http://localhost:8080/feedly/v3/search/feeds/?apikey=A0SXFX54S3K0OC9GNCXG&query=ndtv
+                 * http://sandbox.feedly.com/v3/search/feeds/?apikey=A0SXFX54S3K0OC9GNCXG&query=ndtv
+                 * http://sandbox.feedly.com/v3/search/feeds/?apikey=A0SXFX54S3K0OC9GNCXG&query=NDTV?callback=jQuery111107604972799308598_1415403685859&_=1415403685860
+            */
+        /* working
+            $.getJSON(baseUrl + URI+ "?apikey="+apikey+"&query="+query+ "&callback=?", function(json) {
+
+                //TMDb is nice enough to return a message if nothing was found, so we can base our if statement on this information
+        	console.log(json);
+                if (json != "Nothing found."){
+
+                   //Display the poster and a message announcing the result
+                    console.log(json);
+                }});
+                */
+          
+       /* tried with proxy not working
+        request = new XMLHttpRequest;
+        request.open('GET', baseUrl + URI+ "&query="+query, true);
+        request.send();
+        */
+        
+     // service url  Ajax call with call back but throws unexpected token error======== working
+   /* */   var url = 'http://localhost:8080/feedly/v3/search/feeds/?apikey=A0SXFX54S3K0OC9GNCXG&query=NDTV&callback=?';
+         
+        $.ajax({
+            url: url,
+            jsonpCallback: 'processJSON',
+            dataType: 'jsonp',
+            success: function (result) {
+                // process result
+                $('#result').html(result.ip);
+            },
+            error: function (e) {
+                 // log error in browser
+                console.log(e.message);
+            }
+        }); 
+        
+        /* ajax call using php script to solve cross domain issue: not working
+        var requestData = {
+        	clientId: "sandbox",
+        	apikey: "A0SXFX54S3K0OC9GNCXG",
+        	query : "NDTV"
+        };
+        var request = $.ajax({
+            url: "/php/proxy.php",
+            data: {requrl: "http://sandbox.feedly.com" + $.param(requestData) },
+            dataType: 'json'
+        });
+        request.done(function(msg) {
+            callback(msg);
+        });
+          */
+        
+     // Ajax call to  receive rss feeds
+        
+      // var url = baseUrl + URI+ "?apikey="+apikey+"&query="+query+ "&callback=?";
+            /*    
+               $.ajax({
+        	   type:"GET",
+                   url: url,
+                   jsonpCallback: 'processJSON',
+                   dataType: 'jsonp',
+                   success: function (result) {
+                       // process result
+                       console.log(result);
+                    
+                       $(result).find("result").each(function () {
+                	   var title = $(this).find('title').text();
+                           var url = $(this).find('link').text();
+                       });
+                   },
+                   error: function (e) {
+                        // log error in browser
+                       console.log(e.message);
+                   } 
+                   });*/
+               
+        // using third party proxy YQL and works without any error
         $.get(yql).done(function (rss) {
             $(rss).find("item").each(function () {
                 var title = $(this).find('title').text();
