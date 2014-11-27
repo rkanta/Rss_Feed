faa2af6db0b6edd0b383218bdbff6df0a1c5455f
rkanta <kanta.rajesh@gmail.com>
2014-11-27 11:24:35 -0500

    intial import for NewslineMobile

---
+++ NewslineMobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\rajesh.kanta\AndroidADTWorkspace\NewslineMobile\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\rajesh.kanta\AndroidADTWorkspace\NewslineMobile\platforms\android\CordovaLib\AndroidManifest.xml \
